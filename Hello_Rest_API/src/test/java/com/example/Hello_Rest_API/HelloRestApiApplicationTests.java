554ed8a46720efef3ac0b4a528efed0d41a90547
pravinsinghmanh <pravinsingh.manh@gmail.com>
2025-05-06 11:50:29 +0530

    first project

---
+++ Hello_Rest_API/src/test/java/com/example/Hello_Rest_API/HelloRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hello_Rest_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
